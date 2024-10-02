a3d8c719eb69de2c7baea9f8946abfa5779334ad
Snow <90077988+nieves9003@users.noreply.github.com>
2024-10-01 22:06:08 -0400

    Develop (#3)
    * List tasks and create tasks
    
    * edit task
    
    * Create confirmation dialog and change status to completed
    
    * refactor code
    
    * adding notifications
    
    * refactor App
    
    * Merge branch 'main' into develop

--- src/app/taskManager/task.service.ts
+++ src/app/taskManager/task.service.ts
@@ -24,34 +24,4 @@ export class TaskService {
   updateTask(task: Task): Observable<Task> {
     return this._httpClient.put<Task>(`${this.apiUrl}/tasks/${task.id}`, task);
   }
-
-  deleteTask(id: number): Observable<void> {
-    return this._httpClient.delete<void>(`${this.apiUrl}/tasks/${id}`);
-  }
-
-  // Métodos para gestionar personas
-  getPersons(taskId: number): Observable<Person[]> {
-    return this._httpClient.get<Person[]>(`${this.apiUrl}/tasks/${taskId}/persons`);
-  }
-
-  addPerson(taskId: number, person: Person): Observable<Person> {
-    return this._httpClient.post<Person>(`${this.apiUrl}/tasks/${taskId}/persons`, person);
-  }
-
-  updatePerson(taskId: number, person: Person): Observable<Person> {
-    return this._httpClient.put<Person>(`${this.apiUrl}/tasks/${taskId}/persons/${person.id}`, person);
-  }
-
-  deletePerson(taskId: number, personId: number): Observable<void> {
-    return this._httpClient.delete<void>(`${this.apiUrl}/tasks/${taskId}/persons/${personId}`);
-  }
-
-  // Métodos para gestionar habilidades
-  addSkill(taskId: number, personId: number, skill: string): Observable<Person> {
-    return this._httpClient.post<Person>(`${this.apiUrl}/tasks/${taskId}/persons/${personId}/skills`, { skill });
-  }
-
-  deleteSkill(taskId: number, personId: number, skill: string): Observable<Person> {
-    return this._httpClient.delete<Person>(`${this.apiUrl}/tasks/${taskId}/persons/${personId}/skills/${skill}`);
-  }
 }
